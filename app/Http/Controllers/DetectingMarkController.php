32249701275070f31f3a18cacc03c9af56f4eda7
Aya Sawan <ayasawan658@gmail.com>
2023-07-02 10:20:19 +0200

    comment

---
+++ app/Http/Controllers/DetectingMarkController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\DetectingMarkResource;
+use App\Models\DetectingMark;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class DetectingMarkController extends Controller
+{
+    use  ApiResponseTrait;
+    public function index()
+    {
+        $DetectingMark = DetectingMarkResource::collection(DetectingMark::get());
+        return $this->apiResponse($DetectingMark, 'ok', 200);
+    }
+
+
+    public function store(Request $request)
+    {
+        $input=$request->all();
+        $validator = Validator::make( $input, [
+            'FirstName' => 'required',
+            'LastName' => 'required',
+            'BirthPlace' => 'required',
+            'MatherName' => 'required',
+            'FatherName'=> 'required',
+
+        ]);
+        if ($validator->fails()) {
+            return $this->apiResponse(null, $validator->errors(), 400);
+        }
+        $DetectingMark =DetectingMark::create($request->all());
+
+        if ($DetectingMark) {
+            return $this->apiResponse(new DetectingMarkResource($DetectingMark), 'the DetectingMark  save', 201);
+        }
+        return $this->apiResponse(null, 'the DetectingMark  not save', 400);
+    }
+
+
+    public function show($id)
+    {
+        $DetectingMark= DetectingMark::find($id);
+        if($DetectingMark){
+            return $this->apiResponse(new DetectingMarkResource($DetectingMark) , 'ok' ,200);
+        }
+        return $this->apiResponse(null ,'the DetectingMark not found' ,404);
+
+    }
+
+
+    public function update(Request $request,  $id)
+    {
+        $DetectingMark= DetectingMark::find($id);
+        if(!$DetectingMark)
+        {
+            return $this->apiResponse(null ,'the DetectingMark not found ',404);
+        }
+        $DetectingMark->update($request->all());
+        if($DetectingMark)
+        {
+            return $this->apiResponse(new DetectingMarkResource($DetectingMark) , 'the DetectingMark update',201);
+
+        }
+    }
+
+    public function destroy( $id)
+    {
+        $DetectingMark = DetectingMark::find($id);
+        if(!$DetectingMark)
+        {
+            return $this->apiResponse(null ,'the DetectingMark not found ',404);
+        }
+        $DetectingMark->delete($id);
+        if($DetectingMark)
+            return $this->apiResponse(null ,'the DetectingMark delete ',200);
+    }
+
+}
--- routes/api/user.php
+++ routes/api/user.php
@@ -15,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::prefix("DetectingMark")->group(function (){
+    Route::get('/',[\App\Http\Controllers\DetectingMarkController::class,'index']);
+    Route::get('/{id}',[\App\Http\Controllers\DetectingMarkController::class,'index']);
+
+    Route::post('/',[\App\Http\Controllers\DetectingMarkController::class,'store']);
+    Route::post('update/{id}',[\App\Http\Controllers\DetectingMarkController::class,'update']);
+    Route::post('delete/{id}',[\App\Http\Controllers\DetectingMarkController::class,'destroy']);
+});
+
 Route::prefix("complaints")->group(function (){
     Route::get('/',[\App\Http\Controllers\CommentController::class,'index']);
     Route::post('/',[\App\Http\Controllers\CommentController::class,'store']);
@@ -36,7 +45,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::group( ['prefix' =>'user','middleware' => ['auth:user-api','scopes:user'] ],function(){
     Route::get('logout',[PassportAuthController::class,'logout'])->name('Logout');
     Route::get('userInfo',[PassportAuthController::class,'userInfo'])->name('userInfo');
-
 
     Route::post('update/{id}',[\App\Http\Controllers\PassportAuthController::class,'update_informations_user']);
     Route::post('change/{id}',[\App\Http\Controllers\PassportAuthController::class,'change_password']);
